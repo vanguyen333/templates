651c5ac6b21c24fbec9296287dd147a4b9df6ff5
vanguyen333 <vanguyen333@yahoo.com>
2023-08-08 12:40:40 -0400

    tailwindcss worksk

---
+++ src/components/Navbar1.jsx
@@ -0,0 +1,12 @@
+const Navbar1 = () => {
+  return (
+    <div>
+      <h2 className="text-red-300">Brand Name</h2>
+      <h3>Home</h3>
+      <h3>About</h3>
+      <h3>Contact</h3>
+    </div>
+  );
+};
+
+export default Navbar1;
