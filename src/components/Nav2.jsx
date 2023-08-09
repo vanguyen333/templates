0f80b6d647510f571616261eed10f76910f7a48a
vanguyen333 <vanguyen333@yahoo.com>
2023-08-09 17:39:46 -0400

    create nav2 and nav3

--- src/components/Nav2.jsx
+++ src/components/Nav2.jsx
@@ -1,9 +1,15 @@
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
+import React, { useState } from "react";
 
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 const Nav2 = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
     <div>
       {" "}
@@ -11,11 +17,34 @@ const Nav2 = () => {
         <header>
           <nav className="flex justify-between px-10 bg-blue-400 py-4">
             <h1>Brand Name</h1>
-            <div className="gap-10 flex">
+            <button
+              className="md:hidden text-white"
+              onClick={toggleMobileMenu}
+              aria-label="Toggle Menu"
+            >
+              ☰
+            </button>
+            <div
+              className={`md:flex md:space-x-10 ${
+                isMobileMenuOpen ? "block" : "hidden"
+              }`}
+            >
               <Link to="/">Home</Link>
 
-              <NavLink to="/about">About</NavLink>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink
+                to="/about"
+                onClick={toggleMobileMenu}
+                className="block mt-4 md:inline-block md:mt-0 text-blue-900 hover:text-blue-600"
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/contact"
+                onClick={toggleMobileMenu}
+                className="block mt-4 md:inline-block md:mt-0 text-blue-900 hover:text-blue-600"
+              >
+                Contact
+              </NavLink>
             </div>
           </nav>
         </header>
