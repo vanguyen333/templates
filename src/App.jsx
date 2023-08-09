652bfa16a7aad08adb5f0e2fd23288251c1094bf
vanguyen333 <vanguyen333@yahoo.com>
2023-08-08 22:45:53 -0400

    navbar styling works at the right position, not responsive or react router dom yet

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Navbar1 from "./components/navbar1";
 function App() {
   return (
     <>
-      {/* <Navbar1 /> */}
-      <h1 className="text-3xl font-bold underline text-blue-400">
-        Hello world!
-      </h1>
+      <Navbar1 />
     </>
   );
 }
--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,10 +1,17 @@
 const Navbar1 = () => {
   return (
     <div>
-      <h2 className="text-red-300">Brand Name</h2>
-      <h3>Home</h3>
-      <h3>About</h3>
-      <h3>Contact</h3>
+      {/* justify-between makes the 2 divs have space between, the second div to be on the far left */}
+      <div className="nav flex gap-10 justify-between py-4 my-4 px-10 bg-orange-200 text-lg">
+        <div className="brand">
+          <h1 className="text-red-500 font-bold ">Brand Name</h1>
+        </div>
+        <div className="leftbar flex gap-10 ">
+          <h1>Home</h1>
+          <h1>About</h1>
+          <h1>Contact</h1>
+        </div>
+      </div>
     </div>
   );
 };
